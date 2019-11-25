3d06675d5522f6640e0efc91736306d73c87096d
Łukasz Blachnicki <lblachnicki@gmail.com>
2019-11-25 12:26:28 +0100

    Rewrite lab5 task1 to use BigIntegers and create a solution for lab6

--- Laboratories/src/lab5/Task1.java
+++ Laboratories/src/lab5/Task1.java
@@ -10,34 +10,34 @@ public class Task1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-//
-//		System.out.print("Enter n for factorials: ");
-//		BigInteger n = BigInteger.valueOf(sc.nextInt());
-//
-//		System.out.print("Enter a and b for GCD: ");
-//		BigInteger a = BigInteger.valueOf(sc.nextInt());
-//		BigInteger b = BigInteger.valueOf(sc.nextInt());
+
+		System.out.print("Enter n for factorials: ");
+		BigInteger n = BigInteger.valueOf(sc.nextInt());
+
+		System.out.print("Enter a and b for GCD: ");
+		BigInteger a = BigInteger.valueOf(sc.nextInt());
+		BigInteger b = BigInteger.valueOf(sc.nextInt());
 		System.out.println("Enter the integer array in the following way:");
 		System.out.println("<size> <element1> <element2> ...etc");
 		BigInteger[] arr = ArrayPack.readBigIntegerArray(sc);
 
-//		System.out.println("Iterative factorial of " + n + ": ");
-//		System.out.println(FactorialIterative(n));
-//
-//		System.out.println("Recursive factorial of " + n + ": ");
-//		System.out.println(FactorialRecursive(n));
-//
-//		System.out.println("Iterative GCD of " + a + " and " + b + ": ");
-//		System.out.println(GCDIterative(a, b));
-//
-//		System.out.println("Recursive GCD of " + a + " and " + b + ": ");
-//		System.out.println(GCDRecursive(a, b));
-//
-//		System.out.println("Recursive number of positive values: ");
-//		System.out.println(NumberOfPositiveValuesRecursive(arr));
-//
-//		System.out.println("Iterative number of positive values: ");
-//		System.out.println(NumberOfPositiveValuesIterative(arr));
+		System.out.println("Iterative factorial of " + n + ": ");
+		System.out.println(FactorialIterative(n));
+
+		System.out.println("Recursive factorial of " + n + ": ");
+		System.out.println(FactorialRecursive(n));
+
+		System.out.println("Iterative GCD of " + a + " and " + b + ": ");
+		System.out.println(GCDIterative(a, b));
+
+		System.out.println("Recursive GCD of " + a + " and " + b + ": ");
+		System.out.println(GCDRecursive(a, b));
+
+		System.out.println("Recursive number of positive values: ");
+		System.out.println(NumberOfPositiveValuesRecursive(arr));
+
+		System.out.println("Iterative number of positive values: ");
+		System.out.println(NumberOfPositiveValuesIterative(arr));
 
 		System.out.println("Recursive total of positive values: ");
 		System.out.println(TotalOfPositiveValuesRecursive(arr));
@@ -48,65 +48,72 @@ public class Task1 {
 		sc.close();
 	}
 
-//	public static BigInteger FactorialIterative(BigInteger n) {
-//		if (n <= 0) {
-//			return -1;
-//		}
-//		int result = 1;
-//		for (int i = 1; i <= n; i++) {
-//			result *= i;
-//		}
-//		return result;
-//	}
-//
-//	public static BigInteger FactorialRecursive(BigInteger n) {
-//		if (n >= 1) {
-//			return n * FactorialRecursive(n - 1);
-//		} else {
-//			return 1;
-//		}
-//	}
-//
-//	public static BigInteger GCDRecursive(BigInteger a, BigInteger b) {
-//		if (b != 0) {
-//			return GCDRecursive(b, a % b);
-//		}
-//		return a;
-//	}
-//
-//	public static BigInteger GCDIterative(BigInteger a, BigInteger b) {
-//		int number = 1;
-//		int GCD = 1;
-//		while (number <= a && number <= b) {
-//			if (a % number == 0 && b % number == 0) {
-//				GCD = number;
-//			}
-//			number++;
-//		}
-//		return GCD;
-//	}
-//
-//	public static BigInteger NumberOfPositiveValuesRecursive(BigInteger[] arr) {
-//		int counter = 0;
-//		if (arr.length > 0) {
-//			int[] newArr = Arrays.copyOfRange(arr, 1, arr.length);
-//			if (arr[0] > 0) {
-//				counter++;
-//			}
-//			counter += NumberOfPositiveValuesRecursive(newArr);
-//		}
-//		return counter;
-//	}
-//
-//	public static BigInteger NumberOfPositiveValuesIterative(BigInteger[] arr) {
-//		int counter = 0;
-//		for (int i = 0; i < arr.length; i++) {
-//			if (arr[i] > 0) {
-//				counter++;
-//			}
-//		}
-//		return counter;
-//	}
+	public static BigInteger FactorialIterative(BigInteger n) {
+		if (n.compareTo(BigInteger.valueOf(0)) <= 0) {
+			return BigInteger.valueOf(-1);
+		}
+		BigInteger result = BigInteger.valueOf(1);
+		for (int i = 1; n.compareTo(BigInteger.valueOf(i)) >= 0; i++) {
+			result = result.multiply(BigInteger.valueOf(i));
+		}
+		return result;
+	}
+
+	public static BigInteger FactorialRecursive(BigInteger n) {
+		if (n.compareTo(BigInteger.valueOf(1)) >= 0) {
+			return n.multiply(
+					FactorialRecursive(n.add(BigInteger.valueOf(-1))));
+		} else {
+			return BigInteger.valueOf(1);
+		}
+	}
+
+	public static BigInteger GCDRecursive(BigInteger a, BigInteger b) {
+		if (b.equals(BigInteger.valueOf(0)) == false) {
+			return GCDRecursive(b, a.mod(b));
+		}
+		return a;
+	}
+
+	public static BigInteger GCDIterative(BigInteger a, BigInteger b) {
+		BigInteger number = BigInteger.valueOf(1);
+		BigInteger GCD = BigInteger.valueOf(1);
+		
+		while (number.compareTo(a) <= 0 && number.compareTo(b) <= 0) {
+			if (a.mod(number).equals(BigInteger.valueOf(0))
+					&& b.mod(number).equals(BigInteger.valueOf(0))) {
+				GCD = number;
+			}
+			number = number.add(BigInteger.valueOf(1));
+		}
+		return GCD;
+	}
+
+	public static BigInteger NumberOfPositiveValuesRecursive(BigInteger[] arr) {
+		BigInteger counter = BigInteger.valueOf(0);
+		if(arr.length > 0) {
+			BigInteger[] newArr = Arrays.copyOfRange(arr, 1, arr.length);
+			if(arr[0].compareTo(BigInteger.valueOf(0)) > 0) {
+				counter = counter.add(BigInteger.valueOf(1));
+			}
+			counter = counter.add(NumberOfPositiveValuesRecursive(newArr));
+		}
+		
+		return counter;
+	}
+
+	public static BigInteger NumberOfPositiveValuesIterative(BigInteger[] arr) {
+		
+		BigInteger counter = BigInteger.valueOf(0);
+
+				
+				for (int i = 0; i < arr.length; i++) {
+					if (arr[i].compareTo(BigInteger.valueOf(0)) == 1) { // > 0
+						counter = counter.add(BigInteger.valueOf(1));
+					}
+				}
+		return counter;
+	}
 
 	public static BigInteger TotalOfPositiveValuesRecursive(BigInteger[] arr) {
 		BigInteger total = BigInteger.valueOf(0);
@@ -124,7 +131,7 @@ public class Task1 {
 		BigInteger total = BigInteger.valueOf(0);
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i].compareTo(BigInteger.valueOf(0)) == 1) { // > 0
-				total = total.add(arr[i]); 
+				total = total.add(arr[i]);
 			}
 		}
 		return total;
